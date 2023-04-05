9f66675d6bc5cab1256f09cca68bf40e6b1619fe
Holger Hoefling <hhoeflin@gmail.com>
2023-04-05 07:51:15 +0200

    bump version

--- ckpt/__init__.py
+++ ckpt/__init__.py
@@ -1,6 +1,6 @@
 """A package to create checkpoints in code for easier debugging."""
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .config import get_ckpt_dir, set_ckpt_dir
 from .decorator import ckpt
